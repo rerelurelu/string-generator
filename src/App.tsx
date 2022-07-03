90e3270e5c300cfa04c35952c88246cbbe3a0670
zoniha <awaawapurin@gmail.com>
2022-07-04 01:48:18 +0900

    ✨ feat: Add input field outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
+
+import { InputField } from './components/organisms/inputField/InputField';
 import { Header } from './components/organisms/layout/Header';
 
 export const App: FC = () => {
   return (
     <>
       <Header />
+      <InputField />
     </>
   );
 };
---
+++ src/components/atoms/button/createButton.tsx
@@ -0,0 +1,17 @@
+import { FC, ReactNode } from 'react';
+import { Button } from '@chakra-ui/react';
+
+type Props = {
+  children: ReactNode;
+  onClick: () => void;
+};
+
+export const CreateButton: FC<Props> = (props) => {
+  const { children, onClick } = props;
+
+  return (
+    <Button w={200} bg="purple.500" color="purple.50" _hover={{ opacity: 0.8 }} onClick={onClick}>
+      {children}
+    </Button>
+  );
+};
