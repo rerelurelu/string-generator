162c6400f447b11aca52b759deaa0b2c5d11ecfb
zoniha <awaawapurin@gmail.com>
2022-07-03 22:51:06 +0900

    ✨ feat: Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react';
+import { Header } from './components/organisms/layout/Header';
 
 export const App: FC = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
