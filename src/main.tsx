5119a46d22ef67873463fcdedfe64a5d09206c6f
zoniha <awaawapurin@gmail.com>
2022-07-04 21:28:49 +0900

    ✨ feat: Update input field

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,28 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
 import { App } from './App';
 import theme from './theme/theme';
+import outputCardsReducer from './features/outputCards';
+
+const store = configureStore({
+  reducer: {
+    outputCards: outputCardsReducer,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>
 );
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
