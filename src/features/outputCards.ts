42a2e01e2c80b341ad4cc8d9e1660da809c6f500
zoniha <awaawapurin@gmail.com>
2022-07-04 22:53:03 +0900

    ✨ feat: Add text generation fuc

--- src/features/outputCards.ts
+++ src/features/outputCards.ts
@@ -1,14 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type outputCard = {
+  id: string;
+  digits: string;
+  text: string;
+};
 
 export const outputCardSlice = createSlice({
   name: 'outputCards',
-  initialState: { value: [] },
+  initialState: { value: [] as outputCard[] },
   reducers: {
-    addOutputCard: (state: any, action: any) => {
+    addOutputCard: (state: any, action: PayloadAction<outputCard>) => {
       state.value.push(action.payload);
+    },
+    deleteOutputCard: (state: any, action: any) => {
+      state.value = state.value.filter((outputCard: any) => outputCard.id !== action.payload.id);
     },
   },
 });
 
 export default outputCardSlice.reducer;
-export const { addOutputCard } = outputCardSlice.actions;
+export const { addOutputCard, deleteOutputCard } = outputCardSlice.actions;
